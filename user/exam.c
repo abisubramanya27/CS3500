460e06ed9771ff0a3f2901f527ebfb1b71bf795a
abisubramanya27 <abisubramanya27@gmail.com>
2021-11-26 18:22:53 +0530

    finished proper v1

---
+++ user/exam.c
@@ -0,0 +1,22 @@
+#include <kernel/types.h>
+#include <user/user.h>
+
+// User Program written for Lab Exam
+int
+main(int argc, char **argv)
+{
+  int K = 1;
+  if (setKey(K) < 0) {
+    printf("Error while setting Caesar Cipher Key!! Exiting...");
+    exit(0);
+  }
+  
+  char *str = "GDKKN VNQKC!";
+  printf("%s\n", str);
+  
+  if (setKey(0) < 0) {
+    printf("Error while re-setting Caesar Cipher Key!! Exiting...");
+  }
+
+  exit(0);
+}
